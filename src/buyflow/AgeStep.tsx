bbf126be58ab9c914cce79973d9f36f977b31328
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-03-28 17:50:34 +0100

    add validation to AgeStep

--- src/buyflow/AgeStep.tsx
+++ src/buyflow/AgeStep.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
 
-interface AgeStepProps {
-  cb: (field: string, value: number) => void
+type AgeStepProps = {
+  getStepCallback: (field: string, value: number) => void
 }
 
-const AgeStep: React.FC<AgeStepProps> = (props) => {
+const AgeStep: React.FC<AgeStepProps> = ({getStepCallback}) => {
+  
+  // Set the initial state for the age and error variables
   const [age, setAge] = useState(0)
+  const [error, setError] = useState('')
+
+  // Define a function to validate the age input
+  const validateAge = () => {
+    // Check if the input is a valid age (a positive integer between 18 and 130)
+    const isValidAge = /^[1-9]\d*$/.test(age.toString()) && age >= 18 && age <= 130
+    // Set the error message if the input is invalid
+    setError(isValidAge ? '' : 'Please enter a valid age between 18 and 130')
+    // Return true if the input is valid, false otherwise
+    return isValidAge
+  }
+
   return (
     <>
       <div>
@@ -18,7 +32,8 @@ const AgeStep: React.FC<AgeStepProps> = (props) => {
           value={age}
         ></input>
       </div>
-      <button onClick={() => props.cb('age', age)}>Next</button>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <button onClick={() => validateAge() && getStepCallback('age', age)}>Next</button>
     </>
   )
 }
